5cbe3deed6d0f339acc01bdbac973ab6c0c8a76b
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-06-04 19:30:42 -0400

    Day 1 part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,61 @@
+pub fn parse_elves_caloric_rations(input: &str) -> Vec<Vec<u32>> {
+    input
+        .split("\n\n")
+        .map(|rations_pack| {
+            rations_pack
+                .trim()
+                .lines()
+                .map(|pack_entry| {
+                    pack_entry
+                        .parse()
+                        .unwrap()
+            })
+            .collect()
+        })
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let elf_calorie_packs = parse_elves_caloric_rations(input);
+
+    let result: u32 = elf_calorie_packs
+        .into_iter()
+        .map(|rations_pack| rations_pack.into_iter().sum())
+        .max()
+        .unwrap();
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let elf_calorie_packs = parse_elves_caloric_rations(input);
+
+
+    Some(100)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+
+        assert_eq!(part_one(&input), Some(100));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+
+        assert_eq!(part_two(&input), None);
+    }
+}
